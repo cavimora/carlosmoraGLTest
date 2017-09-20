2450d791af60e32fd4fc1773bc73093ca5304d7b
cavimora <cavimora@gmail.com>
2017-09-19 18:29:05 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Run the project
+- `npm install`
+- `gulp`
+- command prompt 1 type `tsc` to compile typescript files.
+- command prompt 2 type `node server.js`
+
+### Navigate to http://localhost:8000
---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+var server = require('http').Server(app);
+
+var port = 8000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+server.listen(port, () => {
+  console.log("Listening on port " + port);
+});
